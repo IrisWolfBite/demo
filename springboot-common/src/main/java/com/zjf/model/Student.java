8e2965a0c7d69ac90f6c0465fa18cc4339e0700e
zjf <zs@zjf.com>
2019-11-26 09:15:15 +0800

    公共工程(实体类，常量等)

---
+++ springboot-common/src/main/java/com/zjf/model/Student.java
@@ -0,0 +1,55 @@
+package com.zjf.model;
+
+import java.io.Serializable;
+
+public class Student implements Serializable {
+    private Long id;
+
+    private String name;
+
+    private Integer age;
+
+    private String address;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Student(Long id, String name, Integer age, String address) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+    public Student() {
+    }
+}
